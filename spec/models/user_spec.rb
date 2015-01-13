516a3a59f90b0fac3de11d1fa57b27a5591778f1
gouf <innocent.zero@gmail.com>
2015-01-13 11:30:55 +0900

    Add about password and authentication respond_to

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:email) }
   it { is_expected.to respond_to(:password_digest) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:authenticate) }
 
   it { is_expected.to be_valid }
 
