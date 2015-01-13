a9a695dbfc868b5a0655835a4ca2e9efb16a794d
gouf <innocent.zero@gmail.com>
2015-01-13 22:57:30 +0900

    Create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email].to_s.downcase)
+    user_authenticate = user.try(:authenticate, params[:session][:password])
+    if user.present? && user_authenticate.present?
+      sign_in user
+      redirecto_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
