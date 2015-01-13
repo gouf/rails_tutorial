f939566bac137f0197afd87669af8f365a5674a6
gouf <innocent.zero@gmail.com>
2015-01-13 14:02:28 +0900

    Creates user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+User.create(
+  name: 'Michael Hartl', email: 'mhartl@example.com',
+  password: 'foobar', password_confirmation: 'foobar'
+)
