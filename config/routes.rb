ba266d4eb8620d77dc4ddde18fea754aa050fc94
gouf <innocent.zero@gmail.com>
2015-01-13 20:42:41 +0900

    Add sessions resources/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'static_pages#home'
 
   get 'static_pages/home', as: 'home'
 
   match 'signup', to: 'users#new', via: :get
+
+  match '/signin', to: 'sessions#new', via: :get
+
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help', via: :get
 
