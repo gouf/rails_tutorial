03640e15ccd41091ad7ecbe7965e4e9397f805b1
gouf <innocent.zero@gmail.com>
2015-01-13 22:59:07 +0900

    Add signin authentication spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,5 +8,26 @@ RSpec.describe 'AuthenticationPages', type: :request do
 
     it { is_expected.to have_content('Sign in') }
     it { is_expected.to have_title('Sign in') }
+
+    describe 'with invalid information' do
+      before { click_button 'Sign in' }
+
+      it { is_expected.to have_title('Sign in') }
+      it { is_expected.to have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+        it { is_expected.to_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    describe 'with valid information' do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in 'Email',    with: user.email.upcase
+        fill_in 'Password', with: user.password
+        click_button 'Sign in'
+      end
+    end
   end
 end
