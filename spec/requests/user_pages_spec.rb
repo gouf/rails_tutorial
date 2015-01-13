6a4f42edf28121172780817cb5ace8679441e63f
gouf <innocent.zero@gmail.com>
2015-01-13 14:28:27 +0900

    Set spec subject to page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "UserPages", type: :request do
+  subject { page }
+
   describe 'signup page' do
     before { visit signup_path }
     it { have_content('Sign up') }
